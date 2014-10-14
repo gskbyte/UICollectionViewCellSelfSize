4e8b98124f3bc87eb853c6c087704db489281ddc
Jose Alcalá Correa <jose.alcala.correa@gmail.com>
2014-10-14 11:28:23 +0200

    Initial Commit

---
+++ AutoSizingExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoSizingExample
+//
+//  Created by Jose Alcalá-Correa on 14/10/14.
+//  Copyright (c) 2014 gskbyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoSizingExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoSizingExample
+//
+//  Created by Jose Alcalá-Correa on 14/10/14.
+//  Copyright (c) 2014 gskbyte. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
